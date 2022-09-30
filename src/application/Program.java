fd1fe934acfdd933ee58860eadfdfb8a9192cf96
Pedro Dantas <pedrood1232@gmail.com>
2022-09-30 12:36:01 -0300

    Implementation insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -23,5 +24,10 @@ public class Program {
 		System.out.println("\n==== TEST 3: seller findAll ====");
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
+
+		System.out.println("\n==== TEST 4: seller insert ====");
+		Seller newSeller = new Seller(null, "Fabio", "fabio@gmail.com", new Date(), 4300.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 }
